8b88ca00afee023164e00aca8935ab7822d58a09
Alexander Miller <alles.mil@gmail.com>
2020-04-14 15:16:53 +0200

    Add JSDoc

--- src/list.component.js
+++ src/list.component.js
@@ -1,19 +1,31 @@
+/**
+ * A component that fetches data from a service and adds it as children to an element in the DOM.
+ */
 export class ListComponent {
+	/**
+	 * Creates a new ListComponent object
+	 * 
+	 * @param {DOMElement} targetElement 
+	 * @param {ApiService} apiService 
+	 */
 	constructor(targetElement, apiService) {
 		this._targetElement = targetElement;
 		this._apiService = apiService;
 	}
 
+	/**
+	 * Asynchonously get data from the API and add it to the DOM.
+	 */
 	async getAndShowData() {
-		await this.getData();
-		this.showData();
+		await this._getData();
+		this._showData();
 	}
 
-	async getData() {
+	async _getData() {
 		this._data = await this._apiService.getList();
 	}
 
-	showData() {
+	_showData() {
 		for (let dataItem of this._data) {
 			let div = document.createElement('div');
 			div.innerText = dataItem;
